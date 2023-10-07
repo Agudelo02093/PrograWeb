2e0a3766e7a164e0590b7669a59cb1a1bbb579ce
Agudelo02093 <98620510+Agudelo02093@users.noreply.github.com>
2023-10-06 21:02:34 -0500

    Delete Class Projects directory

--- Class Projects/Gift App/main.jsx
+++
@@ -1,8 +0,0 @@
-import './style.css'
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import { App } from './src/App'
-
-const rootElment = document.getElementById('app')
-const root = createRoot(rootElment)
-root.render(<App />)
--- Class Projects/Gift App/src/components/index.js
+++
@@ -1,3 +0,0 @@
-export * from './Header/Header'
-export * from './Form/Form'
-export * from './Gifs/Gifs'
