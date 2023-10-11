81b11d2c0d0f3d5b366f28c8b51c9ea95dee2be2
Agudelo02093 <98620510+Agudelo02093@users.noreply.github.com>
2023-10-11 15:36:05 -0500

    Delete Class Projects directory

--- Class Projects/ToDoList/src/components/Button/Button.jsx
+++
@@ -1,9 +0,0 @@
-import './Button.css'
-
-export function Button ({ labelText, type, onClick }) {
-  return (
-    <button className={`${type}-btn`} type='button' onClick={onClick}>
-      {labelText}
-    </button>
-  )
-}
--- Class Projects/gif-search-app/src/components/Header/Header.jsx
+++
@@ -1,8 +0,0 @@
-import { memo } from 'react'
-export const Header = memo(function Header ({ title }) {
-  return (
-    <header>
-      <h1>{title}</h1>
-    </header>
-  )
-})
