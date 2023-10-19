c3fa50576f5dceaeec61d6d49cfa242e97eb8c01
Agudelo02093 <98620510+Agudelo02093@users.noreply.github.com>
2023-10-19 17:54:51 -0500

    Delete Gaticos_App directory

--- Gaticos_App/src/components/index.js
+++
@@ -1,3 +0,0 @@
-export * from './Button/Button'
-export * from './Header/Header'
-export * from './RandomFact/RandomFact'
--- Gaticos_App/src/context/GaticoContext.js
+++
@@ -1,3 +0,0 @@
-import { createContext } from "react";
-
-export const GaticoContext = createContext(null)
