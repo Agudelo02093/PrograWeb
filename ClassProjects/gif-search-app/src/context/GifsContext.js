4e14402fd4d3dc8d5fcfec25b54316b7a575a4dd
Agudelo02093 <98620510+Agudelo02093@users.noreply.github.com>
2023-10-19 17:52:42 -0500

    Delete ClassProjects directory

--- ClassProjects/gif-search-app/src/context/GifsContext.js
+++
@@ -1,4 +0,0 @@
-import { createContext } from 'react'
-// * 1. Crear el Contexto
-
-export const GifsContext = createContext(null)
--- ClassProjects/gif-search-app/src/models/gif.model.js
+++
@@ -1,7 +0,0 @@
-export class Gif {
-  constructor (id, title, img) {
-    this.id = id
-    this.title = title
-    this.img = img
-  }
-}
