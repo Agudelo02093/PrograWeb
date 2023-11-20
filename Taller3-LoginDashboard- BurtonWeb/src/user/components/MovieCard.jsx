e339786481d84c07d606ed1c9be76a2ebc2fda65
Agudelo02093 <98620510+Agudelo02093@users.noreply.github.com>
2023-11-20 00:54:09 -0500

    Arreglado :)

--- Taller3-LoginDashboard- BurtonWeb/src/user/components/MovieCard.jsx
+++ Taller3-LoginDashboard- BurtonWeb/src/user/components/MovieCard.jsx
@@ -1,68 +1,24 @@
 import { useEffect, useState } from 'react'
-import { useNavigate } from 'react-router-dom'
-import fetchData from '../services/fetchData'
+import { useUser } from '../hooks/useUser'
 import { Filter } from './Filter'
 import Burton from '../../assets/burton.png'
 import Logo from '../../assets/logo.png'
 
 export function MovieCard () {
-    const navigate = useNavigate()
-    const [selectedMovie, setSelectedMovie] = useState(null)
-
-    const handleMovieCardClick = (movie) => {
-        setSelectedMovie(movie);
-        navigate('/detail', { state: { item: movie } });
-    }
-
-    const [popularMovies, setPopularMovies] = useState([]);
-    const [cinema, setCinema] = useState([]);
-    const [selectedFilter, setSelectedFilter] = useState('');
-    const [jobOptions, setJobOptions] = useState([]);
+    
+    const {
+        popularMovies,
+        cinema,
+        selectedFilter,
+        groupedMoviesByJob,
+        setSelectedFilter,
+        handleMovieCardClick,
+        fetchData,
+    } = useUser();
 
     useEffect(() => {
-        fetchData()
-            .then(({ combined_credits }) => {
-                console.log('Créditos combinados:', combined_credits);
-
-                const uniqueJobs = [...new Set(combined_credits.crew.map((member) => member.job))];
-                setJobOptions(uniqueJobs);
-
-                const popularDirectorMovies = combined_credits.crew
-                    .filter((credit) => credit.job === 'Director' && credit.popularity > 50)
-                    .slice(0, 10)
-                    .map((destac) => ({
-                        id: destac.id,
-                        title: destac.title,
-                        posterPath: destac.poster_path,
-                        overview: destac.overview
-                    }))
-                
-                    setPopularMovies(popularDirectorMovies)
-                
-                // Obtener todas las películas
-                const allCrewMovies = combined_credits.crew.map((crewMember) => ({
-                    id: crewMember.id,
-                    title: crewMember.title,
-                    posterPath: crewMember.poster_path,
-                    job: crewMember.job || 'Not clear',
-                    overview: crewMember.overview
-                }));
-
-                setCinema(allCrewMovies);
-            })
-            .catch((error) => {
-                console.error("There aren't data: ", error);
-            })
-    }, [])
-
-    const groupedMoviesByJob = cinema.reduce((acc, movie) => {
-        const job = movie.job || 'Not clear';
-        if (!acc[job]) {
-            acc[job] = [];
-        }
-        acc[job].push(movie);
-        return acc;
-    }, {});
+        fetchData();
+    }, [fetchData]);
 
     return (
         <>
